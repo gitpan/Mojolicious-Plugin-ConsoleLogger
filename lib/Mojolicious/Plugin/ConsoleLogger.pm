9d449c71ac5dc6f311b178142ea9d3ccdac0b152
Glen Hinkle <tempire@cpan.org>
2011-02-22 00:52:34 +0000

    Import of TEMPIRE/Mojolicious-Plugin-ConsoleLogger-0.01 from CPAN.
    gitpan-cpan-distribution: Mojolicious-Plugin-ConsoleLogger
    gitpan-cpan-version:      0.01
    gitpan-cpan-path:         TEMPIRE/Mojolicious-Plugin-ConsoleLogger-0.01.tar.gz
    gitpan-cpan-author:       TEMPIRE
    gitpan-cpan-maturity:     released

---
+++ lib/Mojolicious/Plugin/ConsoleLogger.pm
@@ -0,0 +1,124 @@
+package Mojolicious::Plugin::ConsoleLogger;
+
+use Mojo::Base 'Mojolicious::Plugin';
+use Mojo::ByteStream;
+use Mojo::JSON;
+
+our $VERSION = 0.01;
+
+has logs => sub {
+    return {
+        fatal => [],
+        info  => [],
+        debug => [],
+        error => [],
+    };
+};
+
+sub register {
+    my ($plugin, $app) = @_;
+
+    # override Mojo::Log->log
+    no strict 'refs';
+    my $stash = \%{"Mojo::Log::"};
+    my $orig  = delete $stash->{"log"};
+
+    *{"Mojo::Log::log"} = sub {
+        push @{$plugin->logs->{$_[1]}} => $_[-1];
+
+        # Original Mojo::Log->log
+        $orig->(@_);
+    };
+
+    $app->hook(
+        after_dispatch => sub {
+            my $self = shift;
+            my $logs = $plugin->logs;
+
+            # leave static content untouched
+            return if $self->stash('mojo.static');
+
+            my $str = "\n<!-- Mojolicious logging -->\n<script>";
+
+            for (sort keys %$logs) {
+                next if !@{$logs->{$_}};
+                $str .= "console.group(\"$_\"); ";
+                $str .= _format_msg($_) for @{$logs->{$_}};
+                $str .= "console.groupEnd(\"$_\"); ";
+            }
+
+            $str .= "</script>\n";
+
+            $self->res->body($self->res->body . $str);
+        }
+    );
+}
+
+sub _format_msg {
+    my $msg = shift;
+
+    return "console.log(" . Mojo::JSON->new->encode($_) . "); " if ref $msg;
+
+    return "console.log(" . Mojo::ByteStream->new($_)->quote . "); ";
+}
+
+1;
+
+=head1 NAME
+
+Mojolicious::Plugin::ConsoleLogger - Console logging in your browser
+
+=head1 DESCRIPTION
+
+L<Mojolicious::Plugin::ConsoleLogger> pushes Mojolicious log messages to your browser's console tool.
+
+=head1 USAGE
+
+    use Mojolicious::Lite;
+
+    plugin 'console_logger';
+
+    get '/' => sub {
+
+        app->log->debug("Here I am!");
+        app->log->error("This is bad");
+        app->log->fatal("This is really bad");
+        app->log->info("This isn't bad at all");
+
+        shift->render(text => 'Ahm in ur browzers, logginz ur console');
+    };
+
+    app->start;
+
+=head1 METHODS
+
+L<Mojolicious::Plugin::ConsoleLogger> inherits all methods from
+L<Mojolicious::Plugin> and implements the following new ones.
+
+=head2 C<register>
+
+    $plugin->register;
+
+Register condition in L<Mojolicious> application.
+
+=head1 SEE ALSO
+
+L<Mojolicious>
+
+=head1 DEVELOPMENT
+
+L<http://github.com/tempire/mojolicious-plugin-consolelogger>
+
+=head1 VERSION
+
+0.01
+
+=head1 CREDITS
+
+Implementation stolen from L<Plack::Middleware::ConsoleLogger>
+
+=head1 AUTHOR
+
+Glen Hinkle tempire@cpan.org
+
+=cut
---
+++ t/logger.t
@@ -0,0 +1,56 @@
+use Test::More tests => 17;
+use Test::Mojo;
+
+# Make sure sockets are working
+plan skip_all => 'working sockets required for this test!'
+  unless Mojo::IOLoop->new->generate_port;    # Test server
+
+use Mojolicious::Lite;
+
+plugin 'console_logger';
+
+get '/:template' => sub {
+    my $self = shift;
+    app->log->info('info');
+    app->log->debug('debug');
+    app->log->error('error');
+    app->log->fatal({json => 'structure'});
+
+    $self->render($self->stash->{template})
+      if $self->stash->{template};
+
+    # Template not found, generates exception
+    $self->rendered;
+};
+
+# Tests
+my $client = app->client;
+my $t      = Test::Mojo->new;
+
+# Script tag in dynamic content
+$t->get_ok($_)->status_is(200)->element_exists('script')
+  ->content_like(
+    qr/console\.group\("info"\);.*?console\.log\("info"\);.*?console\.groupEnd\("info"\);/
+  )
+  ->content_like(
+    qr/console\.group\("debug"\);.*?console\.log\("debug"\);.*?console\.groupEnd\("debug"\);/
+  )
+  ->content_like(
+    qr/console\.group\("error"\);.*?console\.log\("error"\);.*?console\.groupEnd\("error"\);/
+  )
+  ->content_like(
+    qr/console\.group\("fatal"\);.*?console\.log\({"json":"structure"}\);.*?console\.groupEnd\("fatal"\);/
+  )
+
+  for qw| /normal /exception |;
+
+# No script tag in static content
+$t->get_ok('/js/prettify.js')->status_is(200)->element_exists(':not(script)');
+
+__DATA__
+
+@@ normal.html.ep
+<html>
+<body>
+</body>
+</html>
